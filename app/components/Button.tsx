7c4216cbb6a3959e65edfc47dcd907bd2f27a434
Anasraza <ar470684@gmail.com>
2025-12-25 18:00:18 +0500

    solved  button props ts error

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,9 +1,8 @@
-import { FC, MouseEventHandler, RefObject } from "react"
+import { FC, MouseEventHandler } from "react"
 
 interface SubmitButtonProps {
   type: "submit"
   title: string,
-  onclick?: never,
   size?: string,
   loading?: boolean
 }
@@ -19,18 +18,17 @@ interface ActionButtonProps {
 type Props = SubmitButtonProps | ActionButtonProps
 
 const Button: FC<Props> = (props) => {
-  const { type = "button", title, size = "md", loading } = props;
+  const { title, size = "md", loading } = props;
 
-  if (type != "submit") {
-    const { onClick } = props;
-
-    return <button type={type} className={`bg-linear-to-tl from-pink-400 to-purple-500 rounded-sm ${size == "md" ? "py-3 px-6" : "py-1.5 px-4"} text-white cursor-pointer outline-none w-fit`} onClick={onClick} disabled={loading}>{title}
-    </button>
+  if (props.type == "submit") {
+    return (
+      <button type="submit" className={`bg-linear-to-tl from-pink-400 to-purple-500 
+        rounded-sm ${size == "md" ? "py-3 px-6" : "py-1.5 px-4"} text-white cursor-pointer outline-none w-fit`} disabled={loading}>{title}</button>
+    )
   }
 
-  return (
-    <button type={type} className={`bg-linear-to-tl from-pink-400 to-purple-500 rounded-sm ${size == "md" ? "py-3 px-6" : "py-1.5 px-4"} text-white cursor-pointer outline-none w-fit`}>{title}</button>
-  )
+  return <button type="button" className={`bg-linear-to-tl from-pink-400 to-purple-500 rounded-sm ${size == "md" ? "py-3 px-6" : "py-1.5 px-4"} text-white cursor-pointer outline-none w-fit`} onClick={props.onClick} disabled={loading}>{title}
+  </button>
 }
 
 export default Button;
